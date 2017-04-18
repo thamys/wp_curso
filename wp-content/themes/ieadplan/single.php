100eb4643fc52b8151fee71c4ed06643475ef6e4
Thamyres Corrêa <Thamyres Corra>
2017-04-18 00:41:42 -0300

    Criando tema

--- wp-content/themes/ieadplan/single.php
+++ wp-content/themes/ieadplan/single.php
@@ -6,7 +6,14 @@
 
 <?php get_header(); ?>
 
-    <div class="container">
+<div class="container">
+<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+			// Include the single post content template.
+			?>
+      
       <!-- START THE FEATURETTES-->
       <div class="row">
         <h1 class="title border-yellow">Como agradar o coração de Deus?</h1>
@@ -27,7 +34,32 @@
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </div>
       </div>
+      <?php
+
+			if ( is_singular( 'attachment' ) ) {
+				// Parent post navigation.
+				the_post_navigation( array(
+					'prev_text' => _x( '<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'twentysixteen' ),
+				) );
+			} elseif ( is_singular( 'post' ) ) {
+				// Previous/next post navigation.
+				the_post_navigation( array(
+					'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentysixteen' ) . '</span> ' .
+						'<span class="screen-reader-text">' . __( 'Next post:', 'twentysixteen' ) . '</span> ' .
+						'<span class="post-title">%title</span>',
+					'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentysixteen' ) . '</span> ' .
+						'<span class="screen-reader-text">' . __( 'Previous post:', 'twentysixteen' ) . '</span> ' .
+						'<span class="post-title">%title</span>',
+				) );
+			}
+
+			// End of the loop.
+		endwhile;
+		?>
+
     </div>
     <!-- /.container-->
+
+    
 
 <?php get_footer(); ?>
